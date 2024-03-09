42d0aa8f86a3e08de4c581b208ee0d4e87d4995e
slr8 <aliosama7133@gmail.com>
2024-03-09 14:10:57 +0200

    refactoring schedule

---
+++ src/UI/AppLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet, useNavigation } from 'react-router-dom'
+import Loader from './Loader';
+import Navbar from './Navbar';
+import ITNavBar from './ITNavBar';
+import AdminNavBar from './AdminNavBar';
+
+
+
+const AppLayout = () => {
+    const navigate = useNavigation();
+    const isLoading = navigate.state === "loading";
+    console.log(navigate);
+    return (
+        <>
+            {isLoading && <Loader />}
+            <Outlet />
+        </>
+    )
+}
+
+export default AppLayout
