b4e824f5661bce1df8c249df3c7095815719a8ad
Ali Osama <93497258+slr8@users.noreply.github.com>
2024-03-09 19:50:52 +0200

    Add files via upload

--- src/Components/TaProfBox/TaProfBox.jsx
+++ src/Components/TaProfBox/TaProfBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TaProfBox = ({ name, dep, info, img }) => {
+const TaProfBox = ({ name, dep, info, img, gh, li, gm, fb }) => {
     return (
-        <div className='relative bg-white w-[24rem] h-[260px] rounded-[12px]'>
-            <img src={img} className='absolute top-[-45px] bg-[#C5D1F2] w-24 h-24 rounded-full p-1 right-[-5px]' />
+        <div className='relative bg-white w-[24rem] h-max rounded-[12px] '>
+            <img src={img ? img : "/Images/TaProfDetails/7_1-removebg-preview.png"} className='absolute top-[-45px] bg-[#C5D1F2] w-24 h-24 rounded-full p-1 right-[-5px]' />
             <div className='ml-5 pt-5'>
                 <p className='font-bold text-2xl'>{name}</p>
                 <p className='text-gray-500'>{dep}</p>
@@ -12,10 +12,10 @@ const TaProfBox = ({ name, dep, info, img }) => {
                 <p className='text-gray-700 text-lg w-[22rem]'>{info}</p>
             </div>
             <div class="flex justify-center items-center gap-10 my-4 ">
-                <a href="#" className='text-3xl text-[#0060E4]'><i class='bx bxl-github'></i></a>
-                <a href="#" className='text-3xl text-[#0060E4]'><i class='bx bxl-linkedin-square'></i></a>
-                <a href="#" className='text-3xl text-[#0060E4]'><i class='bx bx-envelope'></i></a>
-                <a href="#" className='text-3xl text-[#0060E4]'><i class='bx bxl-facebook-circle'></i></a>
+                <a href={gh} target='_blank' className='text-3xl text-[#0060E4]'><i class='bx bxl-github'></i></a>
+                <a href={li} target='_blank' className='text-3xl text-[#0060E4]'><i class='bx bxl-linkedin-square'></i></a>
+                <a href={gm} target='_blank' className='text-3xl text-[#0060E4]'><i class='bx bx-envelope'></i></a>
+                <a href={fb} target='_blank' className='text-3xl text-[#0060E4]'><i class='bx bxl-facebook-circle'></i></a>
             </div>
         </div>
     )
